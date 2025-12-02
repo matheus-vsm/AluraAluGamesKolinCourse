9339370451acfbe065eec5bcbcc0ce632ee2203f
matheus-vsm <scompinha@hotmail.com>
2025-12-01 22:32:40 -0300

    aula 2.3

--- src/main/kotlin/br/com/alura/alugames/modelo/Aluguel.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/Aluguel.kt
@@ -1,10 +1,17 @@
 package br.com.alura.alugames.modelo
 
+import java.time.LocalDate
+import java.time.Period
+
 data class Aluguel(
     val gamer: Gamer,
-    val jogo: Jogo
+    val jogo: Jogo,
+    val dataInicial: LocalDate,
+    val dataFinal: LocalDate
 ) {
+    val valorDoAluguel = jogo.preco * Period.between(dataInicial, dataFinal).days // cálculo simples do valor do aluguel com base na quantidade de dias
+
     override fun toString(): String {
-        return "Aluguel do Jogo ${jogo.titulo} para ${gamer.nome}"
+        return "Aluguel do Jogo ${jogo.titulo} para ${gamer.nome} pelo valor de $valorDoAluguel"
     }
 }
