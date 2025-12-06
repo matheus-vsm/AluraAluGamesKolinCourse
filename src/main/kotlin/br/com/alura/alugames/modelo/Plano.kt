1f015249724f4f888322064ffd62880c622f5aa5
matheus-vsm <scompinha@hotmail.com>
2025-12-06 17:52:05 -0300

    ajuste

--- src/main/kotlin/br/com/alura/alugames/modelo/Plano.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/Plano.kt
@@ -1,6 +1,6 @@
 package br.com.alura.alugames.modelo
 
-// sealed  permite que apenas classes no mesmo package possam herdar desta classe
+// sealed permite que apenas classes no mesmo pacote ou modulo possam herdar desta classe
 sealed class Plano(val tipo: String) {
     // open permite sobrescrita do método em subclasses
     open fun obterValorAluguel(aluguel: Aluguel): Double {
