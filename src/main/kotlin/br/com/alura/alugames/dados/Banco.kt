43c868f97eb51f0fede5b74f0597d85d229a9bb4
matheus-vsm <scompinha@hotmail.com>
2025-12-09 23:24:15 -0300

    aula 2.3

--- src/main/kotlin/br/com/alura/alugames/dados/Banco.kt
+++ src/main/kotlin/br/com/alura/alugames/dados/Banco.kt
@@ -1,18 +1,20 @@
 package br.com.alura.alugames.dados
 
-import br.com.alura.alugames.modelo.Jogo
-import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.SQLException
-
 // object faz com que a classe seja singleton, ou seja, só pode ter uma instância dela
 object Banco {
-    fun obterConexao(): Connection? {
-        return try {
-            DriverManager.getConnection("jdbc:mysql://localhost:3306/alugames", "root", "mysql123")
-        } catch (e: SQLException) {
-            e.printStackTrace()
-            null
-        }
+//    fun obterConexao(): Connection? {
+//        return try {
+//            DriverManager.getConnection("jdbc:mysql://localhost:3306/alugames", "root", "mysql123")
+//        } catch (e: SQLException) {
+//            e.printStackTrace()
+//            null
+//        }
+//    }
+
+    // Configuração do JPA com base no arquivo persistence.xml
+    fun getEntityManager(): EntityManager {
+        val factory: EntityManagerFactory = Persistence.createEntityManagerFactory("alugames")
+        return factory.createEntityManager()
     }
+
 }
