db51c098b4f00e366da8459c79120a920eeafc33
matheus-vsm <scompinha@hotmail.com>
2025-12-12 21:42:10 -0300

    aula 4.9

--- src/main/kotlin/br/com/alura/alugames/modelo/PeriodoAluguel.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/PeriodoAluguel.kt
@@ -1,10 +1,13 @@
 package br.com.alura.alugames.modelo
 
+import net.bytebuddy.asm.Advice
 import java.time.LocalDate
 import java.time.Period
+import javax.persistence.Embeddable
 
+@Embeddable
 data class PeriodoAluguel(
-    val dataInicial: LocalDate,
-    val dataFinal: LocalDate,
+    val dataInicial: LocalDate = LocalDate.now(),
+    val dataFinal: LocalDate = LocalDate.now().plusDays(7),
     val emDias: Int = Period.between(dataInicial, dataFinal).days // cálculo simples do valor do aluguel com base na quantidade de dias
 )
